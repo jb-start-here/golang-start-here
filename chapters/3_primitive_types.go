26156ad0a7c892fc1b0ff1bdf5dc9ea6bfced958
Joel Bandi <joelvivekbandi@gmail.com>
2022-09-05 13:16:08 -0700

    Added types documentation link - this is explanatory enough

---
+++ chapters/3_primitive_types.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// https://go.dev/ref/spec#Types
+
+
+	// Short documentation link
+
+	// the `type` keyword is used to create custom types or create type aliases and 
+	// https://go.dev/ref/spec#Type_definitions
+	// https://go.dev/ref/spec#Type_identity
+
+	type AString = string //alias
+	// AString == string
+	type ANumber int // custom type (although it just points to a int it doesnt create an alias - creates a new type)
+	// ANumber != int
+
+	var a AString = "Hello"
+	var b ANumber = 45
+
+	fmt.Printf("%v is of type %T\n", a, a)
+	fmt.Printf("%v is of type %T\n", b, b)
+
+	// Hello is of type string
+	// 45 is of type main.ANumber
+}
